fdf5452d3f8a48c044304a409e7d3d879cb14118
Display None <200103136@stu.sdu.edu.kz>
2022-09-07 18:05:22 +0600

    ADDDD

--- ranking.js
+++ ranking.js
@@ -1,39 +1,69 @@
-function getData() {
-  const base = 'https://docs.google.com/spreadsheets/d/1FlHBhreAQ5ANk4wPLTGNdqjfPvhDk0Cf_Dv11jf0jzA/gviz/tq?'
-  const output = document.querySelector('.output')
-  const query = encodeURIComponent('Select A,B,C')
-  const url = base + '&tq=' + query;
-  fetch(url)
-  .then(res => res.text())
-  .then(rep =>{
-    const data = JSON.parse(rep.substr(47).slice(0,-2));
-    const row = document.createElement('tr');
-    const thead = document.createElement('thead')
-    output.append(thead);
-    thead.append(row)
-    console.log(data.table.rows)
-    data.table.cols.forEach((heading)=>{
-        const cell = document.createElement('th');
-        cell.textContent = heading.label;
-        row.append(cell);
-    })
-    const tbody = document.createElement('tbody')
-    output.append(tbody)
-    data.table.rows.forEach((main)=>{
-        const container = document.createElement('tr');
-        tbody.append(container);
-        main.c.forEach((ele)=>{
-            const cell = document.createElement('th');
-            cell.textContent = ele?.v;
-            container.append(cell);
-        })
-    })
+async function fetchWithTimeout(resource, options = {}) {
+    const { timeout = 1000 } = options;
     
-  })
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+    const response = await fetch(resource, {
+      ...options,
+      signal: controller.signal  
+    });
+    clearTimeout(id);
+    return response;
+  }
+async function getData() 
+{
+    try {
+        const base = 'https://docs.google.com/spreadsheets/d/1FlHBhreAQ5ANk4wPLTGNdqjfPvhDk0Cf_Dv11jf0jzA/gviz/tq?'
+        const output = document.querySelector('.output')
+        
+        const query = encodeURIComponent('Select A,B,C LIMIT 22 ')
+        const url = base + '&tq=' + query;
+        let data;
+        const response = await fetchWithTimeout(url,{
+            timeout: 10000
+        })
+        .then(res => res.text())
+        .then(rep =>{
+            data = JSON.parse(rep.substr(47).slice(0,-2));
+            output.innerHTML='';
+        })
+
+        
+            const row = document.createElement('tr');
+            const thead = document.createElement('thead')
+            output.append(thead);
+            thead.append(row)
+            console.log(data.table.rows)
+            data.table.cols.forEach((heading)=>{
+                const cell = document.createElement('th');
+                cell.textContent = heading.label;
+                row.append(cell);
+            })
+            const tbody = document.createElement('tbody')
+            output.append(tbody)
+            data.table.rows.forEach((main)=>
+            {
+                const container = document.createElement('tr');
+                tbody.append(container);
+                main.c.forEach((ele)=>
+                {
+                    const cell = document.createElement('th');
+                    cell.textContent = ele?.v;
+                    container.append(cell);
+                    
+                })
+            })
+            // getData()
+        // })
+        
+    } catch (error) {
+        console.log(error.name === 'AbortError');
+    }
+  
+  
 
 }
 
-getData()
 window.addEventListener('keypress', function (e) 
 {
     if (e.key === 'Enter') 
@@ -50,7 +80,7 @@ window.addEventListener('keypress', function (e)
     }
     if (e.code === 'KeyA')
     {
-        this.document.querySelector('tbody').classList.add('tbody_anima')
+        this.document.querySelector('tbody').classList.toggle('tbody_anima')
     }
 
 })
